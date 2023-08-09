3dd90053cccdae2e667e6e587e6688f403d35ab7
Abhijith H <abhijithh496@gmail.com>
2023-08-09 20:47:43 +0530

    uddate

--- senseiai/tailwind.config.js
+++ senseiai/tailwind.config.js
@@ -1,5 +1,4 @@
 
-const {nextui} = require("@nextui-org/react");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -7,7 +6,6 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
 
   ],
   theme: {
@@ -21,5 +19,5 @@ module.exports = {
       }
     },
   },
-  plugins: [nextui()],
+  plugins: [],
 }
